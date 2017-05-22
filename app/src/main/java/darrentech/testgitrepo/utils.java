7bedb655dea879c1e06cbe73e05c2dd36ee2c79b
darrensun80 <darrensun80@163.com>
2017-05-22 11:28:47 +0800

    Add new utils java

---
+++ app/src/main/java/darrentech/testgitrepo/utils.java
@@ -0,0 +1,11 @@
+package darrentech.testgitrepo;
+
+/**
+ * Created by sundaliang1 on 2017/5/22.
+ */
+
+public class utils {
+    public static void SayHello(){
+        System.out.print("hello world");
+    }
+}
